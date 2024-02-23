cb0a91c301a995b512f5eb55f7e339861ae1a72d
ClaytonDewey <clay@dryadmedia.com>
2024-02-23 13:11:48 -0600

    refactor: Rethink code to work on desktop
    work on meganav

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,9 @@
 // Navigation toggle
 const navOpen = document.querySelector('.navbar-toggle');
 const navContainer = document.getElementById('nav-ubs');
-const navClose = document.querySelector('.navigation__close');
+const navClose = document.querySelector('.nav__close');
+
+const dropdownToggle = document.querySelectorAll('.toggle-dropdown');
 
 navOpen.addEventListener('click', (e) => {
   navContainer.classList.add('open');
@@ -11,4 +13,16 @@ navOpen.addEventListener('click', (e) => {
 navClose.addEventListener('click', () => {
   navContainer.classList.remove('open');
   navOpen.setAttribute('aria-expanded', false);
+
+  dropdownToggle.forEach((e) => {
+    e.parentNode.classList.remove('active');
+  });
+});
+
+dropdownToggle.forEach((event) => {
+  event.addEventListener('click', (e) => {
+    e.preventDefault();
+    // console.log(e.target.parentNode);
+    e.target.parentNode.classList.toggle('active');
+  });
 });
