66d195aa3b561a85b1764226e74f403f32363769
ClaytonDewey <clay@dryadmedia.com>
2024-03-02 13:26:46 -0600

    style: Tweak main header search bar

--- app/js/script.js
+++ app/js/script.js
@@ -1,33 +1,32 @@
 // Navigation toggle
-const navOpen = document.querySelector('.navbar-toggle');
-const navContainer = document.getElementById('nav-ubs');
-const navClose = document.querySelector('.nav__close');
+// const navOpen = document.querySelector('.navbar-toggler');
+// const navContainer = document.getElementById('nav-ubs');
+// const navClose = document.querySelector('.nav__close');
 const searchBtn = document.getElementById('search');
 const search = document.querySelector('.search');
 
-const dropdownToggle = document.querySelectorAll('.toggle-dropdown');
+// const dropdownToggle = document.querySelectorAll('.toggle-dropdown');
 
-navOpen.addEventListener('click', (e) => {
-  navContainer.classList.add('open');
-  navOpen.setAttribute('aria-expanded', true);
-});
+// navOpen.addEventListener('click', (e) => {
+//   navContainer.classList.add('open');
+//   navOpen.setAttribute('aria-expanded', true);
+// });
 
-navClose.addEventListener('click', () => {
-  navContainer.classList.remove('open');
-  navOpen.setAttribute('aria-expanded', false);
+// navClose.addEventListener('click', () => {
+//   navContainer.classList.remove('open');
+//   navOpen.setAttribute('aria-expanded', false);
 
-  dropdownToggle.forEach((e) => {
-    e.parentNode.classList.remove('active');
-  });
-});
+//   dropdownToggle.forEach((e) => {
+//     e.parentNode.classList.remove('active');
+//   });
+// });
 
-dropdownToggle.forEach((event) => {
-  event.addEventListener('click', (e) => {
-    e.preventDefault();
-    // console.log(e.target.parentNode);
-    e.target.parentNode.classList.toggle('active');
-  });
-});
+// dropdownToggle.forEach((event) => {
+//   event.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     e.target.parentNode.classList.toggle('active');
+//   });
+// });
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
